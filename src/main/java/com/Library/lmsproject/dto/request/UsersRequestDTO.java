3d84bc36504a6a5f13bd93b85de919ad6e95a639
Duy-Phan-prg <phanduy.prg@gmail.com>
2025-12-09 16:49:12 +0700

    Update UsersRequestDTO.java

--- src/main/java/com/Library/lmsproject/dto/request/UsersRequestDTO.java
+++ src/main/java/com/Library/lmsproject/dto/request/UsersRequestDTO.java
@@ -1,19 +1,19 @@
-//package com.Library.lmsproject.dto.request;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@Builder
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class UsersRequestDTO {
-//    private String email;
-//    private String password;
-//    private String fullName;
-//    private String phone;
-//    private String address;
-//
-//}
+package com.Library.lmsproject.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsersRequestDTO {
+    private String email;
+    private String password;
+    private String fullName;
+    private String phone;
+    private String address;
+
+}
